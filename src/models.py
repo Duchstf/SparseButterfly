8b783c4f0c78ec23e335c03e7557b2202ace61f0
Duc Hoang <minhduc8199@gmail.com>
2024-04-02 12:06:52 -0400

    back up codes

--- src/models.py
+++ src/models.py
@@ -1,18 +1,20 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
-from layers import ButterflyLinear
+from layers import ButterflyLinear, ButterflyGlobalLinear
 
 class ButterflyMlp(nn.Module):
     """ newMlp, similar to Mlp layers in MLP-Mixer but with extra gelu act and low-rank
     """
-    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0., window_size=3, step=1, stripes_1=3, stripes_2=1, block_size=None):
+    def __init__(self, in_features, hidden_features=None, out_features=None,
+                 act_layer=nn.GELU, drop=0.,
+                 window_size=10, step=1, stripes_1=20, stripes_2=10, block_size=(10,10)):
         super().__init__()
         out_features = out_features or in_features
         hidden_features = hidden_features or in_features
-        self.fc1 = ButterflyLinear(784, 784, bias=True, window_size=window_size, stripes=stripes_1, step=step, block_size=block_size)
-        self.fc2 = ButterflyLinear(784, 128, bias=True, window_size=window_size, stripes=stripes_1, step=step, block_size=block_size)
-        self.fc3 = ButterflyLinear(128, 10, bias=True, window_size=window_size, stripes=stripes_2, step=step, block_size=block_size)
+        self.fc1 = ButterflyGlobalLinear(784, 784, bias=True, window_size=window_size, stripes=stripes_1, step=step, block_size=block_size)
+        self.fc2 = ButterflyGlobalLinear(784, 128, bias=True, window_size=window_size, stripes=stripes_1, step=step, block_size=block_size)
+        self.fc3 = ButterflyGlobalLinear(128, 10, bias=True, window_size=window_size, stripes=stripes_2, step=step, block_size=block_size)
         self.act = act_layer()
         
     def forward(self, x):
