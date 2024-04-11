2db24beb40ee5d67894f5cc19e5361dfe5ceff84
Duc Hoang <minhduc8199@gmail.com>
2024-04-11 16:51:08 -0400

    back up codes

--- src/models.py
+++ src/models.py
@@ -36,19 +36,19 @@ class MNIST_Monarch_MLP(nn.Module):
     """
     def __init__(self, in_features, out_features=10):
         super().__init__()
-        self.fc1 = MonarchLinear(in_features, 392, nblocks=10)
-        self.fc2 = MonarchLinear(392, 128,  nblocks=10, bias=True)
-        self.fc3 = MonarchLinear(128, 64,  nblocks=10, bias=True)
-        self.fc4 = MonarchLinear(64, out_features, nblocks=10, bias=True)
+        self.fc1 = MonarchLinear(in_features, 392, nblocks=15)
+        self.fc2 = MonarchLinear(392, 128,  nblocks=15, bias=True)
+        self.fc3 = MonarchLinear(128, 64,  nblocks=15, bias=True)
+        self.fc4 = MonarchLinear(64, out_features, nblocks=15, bias=True)
         
     def forward(self, x):
         x = torch.flatten(x, 1)
         x = self.fc1(x)
-        x = F.relu(x)
+        x = F.gelu(x)
         x = self.fc2(x)
-        x = F.relu(x)
+        x = F.gelu(x)
         x = self.fc3(x)
-        x = F.relu(x)
+        x = F.gelu(x)
         x = self.fc4(x)
         output = F.log_softmax(x, dim=1)
         return output
